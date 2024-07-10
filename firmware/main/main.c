f75147d5a44a777c09e9787419bb745f57dcf7d6
Rijin <rijin113@gmail.com>
2024-07-09 22:30:07 -0400

    generic tasks created; verified 

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -3,21 +3,12 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 
-void task_LED1(void *param);
-void task_LED2(void *param);
-
-void app_main()
-{
-    xTaskCreate(task_LED1, "task1", 4096, NULL, 3, NULL);
-    xTaskCreate(task_LED2, "task2", 4096, NULL, 2, NULL);
-    vTaskStartScheduler();
-}
-
 void task_LED1(void *param)
 {
     while (1)
     {
         printf("this is task 1\n");
+        vTaskDelay(500); // Yield CPU for 500 ms
     }
 
     vTaskDelete(NULL);
@@ -28,6 +19,14 @@ void task_LED2(void *param)
     while (1)
     {
         printf("this is task 2\n");
+        vTaskDelay(1000); // Yield CPU for 1000 ms
     }
+
     vTaskDelete(NULL);
+}
+
+void app_main()
+{
+    xTaskCreate(task_LED1, "task1", 4096, NULL, 3, NULL);
+    xTaskCreate(task_LED2, "task2", 4096, NULL, 2, NULL);
 }
