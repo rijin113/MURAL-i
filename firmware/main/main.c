11dcaa06e04013db593005004e510c25d789969c
Rijin Muralidharan <41874292+rijin113@users.noreply.github.com>
2024-07-18 22:50:28 -0400

    Feature/add all servo tasks for flight (incomplete, part 1) (#4)
    * update rc reciever task delay
    
    * moved all three servos in parallel; one doesn't work (not sure what the problem is)
    
    * changed macro names to flight control names

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -11,12 +11,16 @@
 
 static ServoControl servo_rudder;
 static ServoControl servo_elevator;
+static ServoControl servo_aileron_one;
+static ServoControl servo_aileron_two;
 
 void app_main()
 {
     /* Plane Setup */
     ServoSetup(&servo_rudder, RUDDER);
     ServoSetup(&servo_elevator, ELEVATOR);
+    ServoSetup(&servo_aileron_one, AILERON1);
+    ServoSetup(&servo_aileron_two, AILERON2);
     RCSetup();
 
     /* Tasks for RC Reciever and Transmitter */
@@ -25,4 +29,6 @@ void app_main()
     /* Tasks for Servos (flying controls) */
     xTaskCreate(task_rudder, "StartRudder", 4096, &(servo_rudder.comparator), 3, NULL);
     xTaskCreate(task_elevator, "StartElevator", 4096, &(servo_elevator.comparator), 3, NULL);
+    xTaskCreate(task_aileron_one, "StartAileronOne", 4096, &(servo_aileron_one.comparator), 3, NULL);
+    xTaskCreate(task_aileron_two, "StartAileronTwo", 4096, &(servo_aileron_two.comparator), 3, NULL);
 }
