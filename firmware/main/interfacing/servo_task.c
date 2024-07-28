051a96ee4544049fb1c0d6d86c0193cf82ae8924
Rijin <rijin113@gmail.com>
2024-07-28 16:43:18 -0400

    cleaned up code

--- firmware/main/interfacing/servo_task.c
+++ firmware/main/interfacing/servo_task.c
@@ -6,16 +6,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+/*
+Flight Control Mapping:
+Channel 1 and Channel 2 is used for roll (Ailerons)
+Channel 3 is for plane collective (elevator)
+Channel 4 is for yaw (rudder)
+*/
+
 #define SERVO_MIN_PULSEWIDTH_US 500  // Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH_US 2500  // Maximum pulse width in microsecond
 #define SERVO_MIN_DEGREE        -90   // Minimum angle
 #define SERVO_MAX_DEGREE        90    // Maximum angle
 
-// All the GPIOs below connect to the PWM signal line
-#define SERVO_GPIO_RUDDER            2
-#define SERVO_GPIO_ELEVATOR          5        
-#define SERVO_GPIO_AILERON1          32      
-#define SERVO_GPIO_AILERON2          33       
+// All the GPIOs below connect to the PWM signal line     
+#define SERVO_GPIO_AILERON1          2      
+#define SERVO_GPIO_AILERON2          4     
+#define SERVO_GPIO_ELEVATOR          12     
+#define SERVO_GPIO_RUDDER            13
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
@@ -70,17 +77,17 @@ void ServoSetup(ServoControl *servo, ServoType servo_type)
     mcpwm_generator_config_t generator_config;
     switch (servo_type)
     {
-        case RUDDER:
-            generator_config.gen_gpio_num = SERVO_GPIO_RUDDER;
-            break;
-        case ELEVATOR:
-            generator_config.gen_gpio_num = SERVO_GPIO_ELEVATOR;
-            break;
         case AILERON1:
             generator_config.gen_gpio_num = SERVO_GPIO_AILERON1;
             break;
         case AILERON2:
             generator_config.gen_gpio_num = SERVO_GPIO_AILERON2;
+            break;
+        case ELEVATOR:
+            generator_config.gen_gpio_num = SERVO_GPIO_ELEVATOR;
+            break;
+        case RUDDER:
+            generator_config.gen_gpio_num = SERVO_GPIO_RUDDER;
             break;
         default:
             break;
@@ -102,43 +109,6 @@ void ServoSetup(ServoControl *servo, ServoType servo_type)
     mcpwm_timer_start_stop(servo->timer, MCPWM_TIMER_START_NO_STOP);
 }
 
-void task_rudder(void *pvParameters)
-{
-    mcpwm_cmpr_handle_t *task_comparator = (mcpwm_cmpr_handle_t *)pvParameters;
-
-    while (1) {
-        ESP_LOGE(SERVO_TAG, "MOVING RUDDER");
-        
-        // TODO: Change all channel indexes below to match actual flight controls.
-        // For example, channel 2 might not actually be used for rudder. 
-
-        // Linear Interpolation ([1000, 2000] to [-90, 90])
-        int rc_value = -270 + ((channels[0]) * 180 / 1000); // Linear interpolation
-
-        mcpwm_comparator_set_compare_value(*task_comparator, angle_to_compare(rc_value));
-
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
-    vTaskDelete(NULL);
-}
-
-void task_elevator(void *pvParameters)
-{
-    mcpwm_cmpr_handle_t *task_comparator = (mcpwm_cmpr_handle_t *)pvParameters;
-
-    while (1) {
-        ESP_LOGE(SERVO_TAG, "MOVING ELEVATOR");
-
-        // Linear Interpolation ([1000, 2000] to [-90, 90])
-        int rc_value = -270 + ((channels[1]) * 180 / 1000); // Linear interpolation
-
-        mcpwm_comparator_set_compare_value(*task_comparator, angle_to_compare(rc_value));
-
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
-    vTaskDelete(NULL);
-}
-
 void task_aileron_one(void *pvParameters)
 {
     mcpwm_cmpr_handle_t *task_comparator = (mcpwm_cmpr_handle_t *)pvParameters;
@@ -147,11 +117,11 @@ void task_aileron_one(void *pvParameters)
         ESP_LOGE(SERVO_TAG, "MOVING AILERON ONE");
 
         // Linear Interpolation ([1000, 2000] to [-90, 90])
-        int rc_value = -270 + ((channels[2]) * 180 / 1000); // Linear interpolation
+        int rc_value = -270 + ((channels[0]) * 180 / 1000); // Linear interpolation
 
         mcpwm_comparator_set_compare_value(*task_comparator, angle_to_compare(rc_value));
 
-        vTaskDelay(pdMS_TO_TICKS(100));
+        // vTaskDelay(pdMS_TO_TICKS(100));
     }
     vTaskDelete(NULL);
 }
@@ -164,12 +134,55 @@ void task_aileron_two(void *pvParameters)
         ESP_LOGE(SERVO_TAG, "MOVING AILERON TWO");
 
         // Linear Interpolation ([1000, 2000] to [-90, 90])
-        int rc_value = -270 + ((channels[3]) * 180 / 1000); // Linear interpolation
+        int rc_value = -270 + ((channels[1]) * 180 / 1000); // Linear interpolation
         ESP_LOGE(SERVO_TAG, "AILERON TWO DATA: %d", rc_value);
 
         mcpwm_comparator_set_compare_value(*task_comparator, angle_to_compare(rc_value));
 
-        vTaskDelay(pdMS_TO_TICKS(50));
+        // vTaskDelay(pdMS_TO_TICKS(50));
+    }
+    vTaskDelete(NULL);
+}
+
+void task_elevator(void *pvParameters)
+{
+    mcpwm_cmpr_handle_t *task_comparator = (mcpwm_cmpr_handle_t *)pvParameters;
+
+    while (1) {
+        ESP_LOGE(SERVO_TAG, "MOVING ELEVATOR");
+
+        // Linear Interpolation ([1000, 2000] to [-90, 90])
+        int rc_value = -1*(-270 + ((channels[2]) * 180 / 1000)); // Linear interpolation
+
+        ESP_LOGE(SERVO_TAG, "ELEVATOR RC VALUE: %d", rc_value);
+
+        mcpwm_comparator_set_compare_value(*task_comparator, angle_to_compare(rc_value));
+
+        // vTaskDelay(pdMS_TO_TICKS(50));
+    }
+    vTaskDelete(NULL);
+}
+
+void task_rudder(void *pvParameters)
+{
+    mcpwm_cmpr_handle_t *task_comparator = (mcpwm_cmpr_handle_t *)pvParameters;
+
+    if (*task_comparator == NULL)
+    {
+        ESP_LOGE(SERVO_TAG, "TASK COMPARATOR IS NULL");
+    }
+
+    while (1) {
+        ESP_LOGE(SERVO_TAG, "MOVING RUDDER");
+
+        // Linear Interpolation ([1000, 2000] to [-90, 90])
+        int rc_value = -270 + ((channels[3]) * 180 / 1000); // Linear interpolation
+
+        ESP_LOGE(SERVO_TAG, "RUDDER RC VALUE: %d", rc_value);
+
+        mcpwm_comparator_set_compare_value(*task_comparator, angle_to_compare(rc_value));
+
+        // vTaskDelay(pdMS_TO_TICKS(50));
     }
     vTaskDelete(NULL);
 }
--- firmware/main/main.c
+++ firmware/main/main.c
@@ -17,18 +17,18 @@ static ServoControl servo_aileron_two;
 void app_main()
 {
     /* Plane Setup */
-    ServoSetup(&servo_rudder, RUDDER);
-    ServoSetup(&servo_elevator, ELEVATOR);
     ServoSetup(&servo_aileron_one, AILERON1);
     ServoSetup(&servo_aileron_two, AILERON2);
+    ServoSetup(&servo_rudder, RUDDER);
+    ServoSetup(&servo_elevator, ELEVATOR);
     RCSetup();
 
     /* Tasks for RC Reciever and Transmitter */
     xTaskCreate(rx_rc_task, "UART_RX_Task", 4096, NULL, configMAX_PRIORITIES, NULL);
 
     /* Tasks for Servos (flying controls) */
-    xTaskCreate(task_rudder, "StartRudder", 4096, &(servo_rudder.comparator), 3, NULL);
-    xTaskCreate(task_elevator, "StartElevator", 4096, &(servo_elevator.comparator), 3, NULL);
     xTaskCreate(task_aileron_one, "StartAileronOne", 4096, &(servo_aileron_one.comparator), 3, NULL);
     xTaskCreate(task_aileron_two, "StartAileronTwo", 4096, &(servo_aileron_two.comparator), 3, NULL);
+    xTaskCreate(task_rudder, "StartRudder", 4096, &(servo_rudder.comparator), 3, NULL);
+    xTaskCreate(task_elevator, "StartElevator", 4096, &(servo_elevator.comparator), 3, NULL);
 }
